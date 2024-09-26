1a427cd1fe629ca6925e344ade9e5c6822c7e7db
Vadim Kurnyshov <vaku878@gmail.com>
2024-09-26 15:17:18 +0400

    add hw-17

---
+++ src/main/resources/script.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS test CASCADE;
+DROP TABLE IF EXISTS question CASCADE;
+DROP TABLE IF EXISTS answer CASCADE;
+
+CREATE TABLE test(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    test_name TEXT NOT NULL
+);
+
+CREATE TABLE question(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    test_id UUID NOT NULL,
+    question_name TEXT NOT NULL
+);
+
+CREATE TABLE answer(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    question_id UUID NOT NULL,
+    answer_name TEXT NOT NULL,
+    is_correct BOOL NOT NULL
+);
+
+ALTER TABLE question ADD CONSTRAINT question_test_id_fkey FOREIGN KEY (test_id) REFERENCES test(id);
+ALTER TABLE answer ADD CONSTRAINT answer_question_id_fkey FOREIGN KEY (question_id) REFERENCES question(id);
